85ec7b90116f6f1b105b2fcad118c001d6736038
Gourab Mahapatra <mgourab@hotmail.com>
2024-03-29 13:45:12 +0530

    22-css-modules-updated

--- 22. CSS Modules/css-modules/src/components/Item.jsx
+++ 22. CSS Modules/css-modules/src/components/Item.jsx
@@ -1,7 +1,6 @@
 import styles from "./Item.module.css";
 
 function Item({ foodItem }) {
-  console.log(styles);
   return (
     <li className={`${styles["kg-item"]} list-group-item`}>
       <span className={`${styles["kg-span"]}`}>{foodItem}</span>
